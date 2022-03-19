c8eb4bb2e72e7369e67ae23edd71edb29684b051
juliannaPeace <juliannapazlo@gmail.com>
2022-03-19 14:33:42 -0300

    adicionando binder do kafka;logando dados do header;

--- src/main/java/com/kafka/resilience/stream/StreamApplication.java
+++ src/main/java/com/kafka/resilience/stream/StreamApplication.java
@@ -2,9 +2,7 @@ package com.kafka.resilience.stream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @SpringBootApplication
 public class StreamApplication {
 
--- src/main/java/com/kafka/resilience/stream/listen/VendaConsumer.java
+++ src/main/java/com/kafka/resilience/stream/listen/VendaConsumer.java
@@ -1,18 +1,27 @@
 package com.kafka.resilience.stream.listen;
 
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.util.function.Consumer;
 
 @Component("vendasListener")
-public class VendaConsumer implements Consumer<VendaPayload> {
+public class VendaConsumer implements Consumer<Message<VendaPayload>> {
 
     @Override
-    public void accept(final VendaPayload vendaPayload) {
-        System.out.println("Descrição: " + vendaPayload.descricao);
-        System.out.println("Valor: " + vendaPayload.valor);
-        System.out.println("Loja: " + vendaPayload.loja);
-        System.out.println("Desconto: " + vendaPayload.desconto);
-        System.out.println("Valor total: " + vendaPayload.valorTotal);
+    public void accept(final Message<VendaPayload> vendaPayload) {
+
+        System.out.println("Descrição: " + vendaPayload.getPayload().descricao);
+        System.out.println("Valor: " + vendaPayload.getPayload().valor);
+        System.out.println("Loja: " + vendaPayload.getPayload().loja);
+        System.out.println("Desconto: " + vendaPayload.getPayload().desconto);
+        System.out.println("Valor total: " + vendaPayload.getPayload().valorTotal);
+        System.out.println("-------------- Headers -------------------");
+        System.out.println("service name: " + new String((byte[]) vendaPayload.getHeaders().get("service_name"), StandardCharsets.UTF_8));
+        System.out.println("correlation id: " + new String((byte[]) vendaPayload.getHeaders().get("correlation_id"), StandardCharsets.UTF_8));
+        System.out.println("id: " + vendaPayload.getHeaders().getId());
+        System.out.println("offset: " + vendaPayload.getHeaders().get("kafka_offset"));
+
     }
 }
